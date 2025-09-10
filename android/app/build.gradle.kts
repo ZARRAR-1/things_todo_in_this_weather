042228fd0d6267256f264c093e8aa186ee1f9413
ZARRAR-1 <mack4009@gmail.com>
2025-09-10 13:40:00 +0500

    Added changes in core android files to enable geolocator plugin work smoothly.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.things_todo_in_this_weather"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
